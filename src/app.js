686ad98018b1b2f0bc7db017c5c220ecb1291f57
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-08 22:15:45 -0300

    chore: set app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import router from "./routes/index.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log("server running on PORT " + PORT);
+});
