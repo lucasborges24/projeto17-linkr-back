799074eaca31893d59372846046bd7d5d703856d
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-10 12:11:30 -0300

    feat: create signin controller function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,17 @@
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
-import { insertUserinUsers } from "../repositories/authRepository.js";
+import {
+  getUserByEmail,
+  insertUserinUsers,
+} from "../repositories/authRepository.js";
 
+dotenv.config();
+const EXPIRE_TIME_SECONDS = 60 * 60;
+const jwtExpire = {
+  expiresIn: EXPIRE_TIME_SECONDS,
+};
 const SALT = 10;
 
 export const signup = async (req, res) => {
@@ -11,6 +21,22 @@ export const signup = async (req, res) => {
   try {
     const insered = await insertUserinUsers(newUser);
     return res.status(201).send("User registered!");
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Internal system error.\n More details: ${error.message}`);
+  }
+};
+
+export const signin = async (req, res) => {
+  const { email } = res.locals.body;
+  try {
+    const { rows: user } = await getUserByEmail(email);
+    const { id: userId } = user[0];
+    const data = { userId };
+    const { JWT_SECRETKEY } = process.env;
+    const token = jwt.sign(data, JWT_SECRETKEY, jwtExpire);
+    res.status(200).send(token);
   } catch (error) {
     res
       .status(500)
