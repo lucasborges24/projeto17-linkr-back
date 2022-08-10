30f7175f0795b1dff952fbfced02c9f1caafa0bd
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-08-10 12:15:19 -0300

    feat: create signin POST route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
-import { signUpSchema } from "../schemas/authSchema.js";
-import { validateSchema } from "../middlewares/schemaMiddleware.js";
+import { signInSchema, signUpSchema, tokenSchema } from "../schemas/authSchema.js";
+import { validateHeaderSchema, validateSchema } from "../middlewares/schemaMiddleware.js";
 import {
   checkEmailAlreadyExists,
+  checkPasswordByEmail,
   checkUsernameAlreadyExists,
 } from "../middlewares/authMiddleware.js";
-import { signup } from "../controllers/authController.js";
+import { signin, signup } from "../controllers/authController.js";
+import { checkTokenBelongsSomeUser } from "../middlewares/tokenMiddleware.js";
 
 const authRouter = Router();
 
@@ -17,6 +19,11 @@ authRouter.post(
   checkUsernameAlreadyExists,
   signup
 );
-authRouter.post("/signin", (req, res) => res.send("funfei de boas"));
+authRouter.post(
+  "/signin",
+  validateSchema(signInSchema),
+  checkPasswordByEmail,
+  signin
+);
 
 export { authRouter };
