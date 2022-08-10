264a809a2cd0988786187f034bc1151c6bd16d78
CaioPedras <caio.vp@gmail.com>
2022-08-10 13:39:06 -0300

    fix:send message on status 404 + magicnumber limit

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -6,7 +6,9 @@ export async function getPostsByHashtag(req, res) {
     const { rows: hashtags } = await hashtagReposity.getHashtageByName(hashtag);
     const [validateHashtag] = hashtags;
     if (!validateHashtag) {
-      return res.sendStatus(404);
+      return res
+        .status(404)
+        .send(`Não foi encontrado posts com a tag referente a ${hashtag}`);
     }
     const { rows: result } = await hashtagReposity.getPostsByHashtag(hashtag);
     return res.status(200).send(result);
@@ -17,8 +19,11 @@ export async function getPostsByHashtag(req, res) {
 }
 
 export async function getTopHashtags(req, res) {
+  const MOST_VIEWED_HASHTAGS_LIMIT = 10;
   try {
-    const { rows: hashtags } = await hashtagReposity.getMostUsedHashtags(10);
+    const { rows: hashtags } = await hashtagReposity.getMostUsedHashtags(
+      MOST_VIEWED_HASHTAGS_LIMIT
+    );
     return res.status(200).send(hashtags);
   } catch (err) {
     console.log(err);
