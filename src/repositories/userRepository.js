c3a1936b6b9e6718aea2b0e677af825e81b19c3c
andreseichi <andre_seichi@hotmail.com>
2022-08-11 21:34:52 -0300

    feat: get all posts from a user by id

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,9 +1,40 @@
 import connection from "../databases/postgres.js";
 
+export const getUser = async (id) => {
+  const sql = `--sql
+  SELECT
+    id,
+    username,
+    email,
+    picture,
+    "createdAt"
+  FROM
+    users
+  WHERE
+    id = $1;
+  `;
+
+  return await connection.query(sql, [id]);
+};
+
 export const getUserPostsById = async (id) => {
   const sql = `--sql
-  select users.username, users.picture, posts.* from users join posts on posts."writerid" = users.id
-where users.id = $1;`;
+  SELECT
+    users.username,
+    users.picture,
+    count("likesPosts".id) as likes,
+    posts.description,
+    posts.url
+  FROM
+    users
+    JOIN posts ON posts."writerId" = users.id
+    LEFT JOIN "likesPosts" ON "likesPosts"."postId" = posts.id
+  WHERE
+    users.id = $1
+  GROUP BY
+    posts.id,
+    users.id
+  `;
 
   return await connection.query(sql, [id]);
 };
@@ -13,7 +44,6 @@ export const searchUsers = async (username) => {
   SELECT users."username", users.picture
   FROM users
   WHERE users."username" ILIKE $1;`;
-
 
   return await connection.query(sql, [`%${username}%`]);
 };
