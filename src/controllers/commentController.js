9df89add62dc638944073446017a8cce5cae1a1b
CaioPedras <caio.vp@gmail.com>
2022-08-16 14:09:06 -0300

    feat: comment controller

---
+++ src/controllers/commentController.js
@@ -0,0 +1,36 @@
+// import { postRepository, hashtagReposity } from "../repositories/index.js";
+
+import {
+  getPostComments,
+  insertComment,
+} from "../repositories/commentRepository.js";
+import { postRepository } from "../repositories/index.js";
+
+export async function postComment(req, res) {
+  const { postId } = req.params;
+  const userId = res.locals.userId;
+  const { comment } = req.body;
+  try {
+    const { rows: post } = await postRepository.getPostById(postId);
+    if (post.length < 1) return res.status(404).send("Post not found");
+    await insertComment(userId, postId, comment);
+
+    res.status(201).send("Comment posted successfully");
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
+export async function getCommentsByPostId(req, res) {
+  const { postId } = req.params;
+  try {
+    const { rows: post } = await postRepository.getPostById(postId);
+    if (post.length < 1) return res.status(404).send("Post not found");
+    const { rows: comments } = await getPostComments(postId);
+
+    res.status(200).send(comments);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
